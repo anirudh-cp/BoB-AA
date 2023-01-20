53a528e78ab8dbf1308b231b66400ef911b42691
Anirudh C P <74400035+anirudh-cp@users.noreply.github.com>
2023-01-20 21:54:25 +0530

    Additional Endpoints

--- app/main.py
+++ app/main.py
@@ -3,13 +3,147 @@ from flask import Flask, make_response, request, redirect
 from flask_pymongo import pymongo
 from bson import json_util
 import json
-
 CONNECTION_STRING = "mongodb+srv://root:bobaaserver@cluster0.ap5vvaf.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('flask_mongodb_atlas')
     
     
 app = Flask(__name__)
+# Debt - emi, moratorium ; Invest - high, medium, low ; Loan - common ; Insurance - common
+
+#insurance
+# vehicle - idv, year ; health - age, premium ; travel - duration, national/intl ; life - duration, premimum
+@app.route("/insurance/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def insurance(phone):
+    
+    if request.method == 'POST':
+        type = request.form["type"]
+        val1 = request.form["val1"]
+        val2 = request.form["val2"]
+        #UPDATE CALCULATION
+        premium = val1 * val2
+        tenure = request.form["tenure"]
+        db.insurance.insert_one({"phone": phone, "type" : type, "premium" : premium, "tenure" : tenure})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['insurance'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['insurance'].delete({'phone': phone})
+
+#loan
+@app.route("/loan/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def loan(phone):
+    
+    if request.method == 'POST':
+        type = request.form["type"]
+        amount = request.form["amount"]
+        tenure = request.form["tenure"]
+        #UPDATE CALCULATION
+        bob = {"amount" : amount, "interest" : interest, "tenure" : tenure, "condition" : conditino}
+        hdfc = {"amount" : amount, "interest" : interest, "tenure" : tenure, "condition" : conditino}
+        sbi = {"amount" : amount, "interest" : interest, "tenure" : tenure, "condition" : conditino}
+        db.loan.insert_one({"phone": phone, "type" : type, "bob" : bob, "hdfc" : hdfc, "sbi" : sbi})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['loan'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['loan'].delete({'phone': phone})
+
+
+#invest page
+@app.route("/invest/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def invest(phone):
+    
+    if request.method == 'POST':
+        risk = request.form["risk"]
+        cost = request.form["cost"]
+        nod = request.form["nod"]
+        #UPDATE CALCULATION
+        totalamt = cost
+        percinvest = cost
+        primary = {"name" : "recurring deposit", "value" : "75K", "perc" : "50%"}
+        secondary = {"name" : "term deposit", "value" : "60K", "perc" : "40%"}
+        tertiary = {"name" : "mutual funds", "value" : "15K", "perc" : "10%"}
+        db.invest.insert_one({"phone": phone, "risk" : risk, "total" : total, "percinvest" : percinvest, "primary" : primary, "secondary" : secondary, "tertiary" : tertiary})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['invest'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['invest'].delete({'phone': phone})
+
+#Debt page, moratorium tab
+@app.route("/moratorium/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def moratorium(phone):
+    
+    if request.method == 'POST':
+        principal = request.form["principal"]
+        interest = request.form["interest"]
+        duration = request.form["duration"]
+        newEmi = principal * interest * duration #UPDATE CALCULATION
+        db.moratorium.insert_one({"phone": phone, "newEmi" : newEmi})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['moratorium'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['moratorium'].delete({'phone': phone})
+
+#Debt page, emi tab
+@app.route("/emi/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def emi(phone):
+    
+    if request.method == 'POST':
+        principal = request.form["principal"]
+        interest = request.form["interest"]
+        tenure = request.form["tenure"]
+        p = principal
+        r = interest/1200
+        n = tenure
+        emi = (p*r*pow((1+r),n)) / (pow((1+r),n)-1)
+        db.emi.insert_one({"phone" : phone,"interest": interest, "tenure": tenure, "emi": emi})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['emi'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['emi'].delete({'phone': phone})
+
+# Home page, goals tab 
+@app.route("/info/<phone>", methods = ['POST', 'GET', 'DELETE'])
+def info(phone):
+    
+    if request.method == 'POST':
+        action = request.form["action"]
+        compare = request.form["compare"]
+        value = request.form["value"]
+        db.info.insert_one({"phone" : phone, "action": action, "compare": compare, "value":value})
+        return make_response('', 200)
+    elif request.method == 'GET':
+        records = db['info'].find({"phone" : phone})
+        if records:
+            return make_response(json.loads(json_util.dumps(records)), 200)
+        else:
+            return make_response({'Data not found'}, 404)
+    elif request.method == 'DELETE':
+        records = db['info'].delete({'phone': phone})
 
 
 @app.route("/redirect", methods = ['POST'])
@@ -31,41 +165,6 @@ def get_redirect(name):
     else:
         return make_response('', 404)
  
- 
-@app.route("/goals", methods = ['POST', 'GET'])
-def goals():
-    
-    if request.method == 'POST':
-        source = request.form["source"]
-        action = request.form["action"]
-        comparision = request.form['comparision']
-        db.goals.insert_one({"source": source, "action": action, "comparision": comparision})
-        return make_response('', 200)
-    elif request.method == 'GET':
-        records = db['goals'].find({})
-        if records:
-            return make_response(json.loads(json_util.dumps(records)), 200)
-        else:
-            return make_response({'Data not found'}, 404)
-
- 
-@app.route("/escorw", methods = ['POST', 'GET'])
-def escrow():
-    
-    if request.method == 'POST':
-        client1 = request.form["client1"]
-        client2 = request.form["client2"]
-        cost = request.form['cost']
-        share_name = request.form['share_name']
-        db.escrow.insert_one({"client1": client1, "client2": client2, "cost": cost, "share_name": share_name})
-        return make_response('', 200)
-    elif request.method == 'GET':
-        records = db['escrow'].find({})
-        if records:
-            return make_response(json.loads(json_util.dumps(records)), 200)
-        else:
-            return make_response({'Data not found'}, 404)
-
 
 @app.route("/rewards", methods = ['POST', 'GET'])
 def rewards():
